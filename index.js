eab6b257903f49abc2ec20fa6438e85dc437e64a
Prayoga BRD <103347873+prayogabrd@users.noreply.github.com>
2022-11-17 22:43:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $('body').css('margin', 0);
+  $('body').html('<iframe src="//insapgan.zidnirifan.my.id/"><iframe>');
+  $('iframe').css({
+    'border': 0,
+    'width': '100vw',
+    'height': '100vh'
+  });
+});
