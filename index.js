30dd3315641af44003993743edf3d8ab4372fa5a
Prayoga BRD <103347873+prayogabrd@users.noreply.github.com>
2022-11-26 15:48:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   $('body').css('margin', 0);
-  $('body').html('<iframe src="//insapgan.zidnirifan.my.id/"><iframe>');
+  $('body').html('<iframe src="//cdn.gamesnacks.com/@famobi_api2/chessclassic4/gameCode/index.html"><iframe>');
   $('iframe').css({
     'border': 0,
     'width': '100vw',
